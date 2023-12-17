1fe26ac8bcb1734746043b27fe6b162249b23ed8
SamuelDhandraj <152612131+SamuelDhandraj@users.noreply.github.com>
2023-12-17 19:53:30 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Style/card.css";
+import Group from "./Components/Price";
+
+
+
+function App() {
+  return <div className="Price">
+  <Group />
+  
+  
+  </div>;
+}
+
+export default App;
